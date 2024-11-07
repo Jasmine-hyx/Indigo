18ebbff3146e471d6245d06f5400717d32f4d3a5
Jasmine-hyx <yh3767@columbia.edu>
2024-11-07 16:24:16 -0500

    changes

---
+++ Exercises/week-09/script.js
@@ -0,0 +1,11 @@
+/*
+let num01 = 10
+let num02 = 20
+
+*/
+console.log("Hello World")
+document.write("Hello World")
+
+let fruits=['apple','banana','orange','kiwi','grass'];
+for(let i=0; i<fruits.length;i++)
+    console.log(fruits[i]);
